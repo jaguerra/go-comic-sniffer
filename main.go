e96373ac431635e775405e4ea29d2f9eb83d412d
Jose Antonio Guerra <jaguerra@tuenti.com>
2020-04-13 17:39:01 +0200

    Some improvements WIP

--- main.go
+++ main.go
@@ -3,21 +3,27 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"github.com/ericchiang/css"
-	"golang.org/x/net/html"
 	"html/template"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/ericchiang/css"
+	"golang.org/x/net/html"
 )
 
 func main() {
-	log.Print("Starting server...")
+	port, portPresent := os.LookupEnv("PORT")
+	if !portPresent {
+		log.Fatal("PORT is not defined, exiting")
+		os.Exit(-1)
+	}
+	log.Print("Starting server on port " + port + " ...")
 	http.HandleFunc("/", defaultHandler)
 	http.HandleFunc("/random", snifferHandler)
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,26 +32,12 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 
 func snifferHandler(w http.ResponseWriter, r *http.Request) {
 
-	req, err := http.NewRequest("GET", os.Getenv("URL"), nil)
+	imageTag, err := getImageFromRemote()
 	if err != nil {
 		errorResponse(&w, err)
 		return
 	}
 
-	ageCookie := http.Cookie{
-		Name:  "age-gated",
-		Value: os.Getenv("AGE_COOKIE"),
-	}
-	req.AddCookie(&ageCookie)
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		errorResponse(&w, err)
-		return
-	}
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	imageTag, err := sniffImage(body)
 	t, err := template.ParseFiles("random.html")
 	if err != nil {
 		errorResponse(&w, err)
@@ -57,6 +49,27 @@ func snifferHandler(w http.ResponseWriter, r *http.Request) {
 		errorResponse(&w, err)
 		return
 	}
+}
+
+func getImageFromRemote() (imageTag string, reason error) {
+	req, err := http.NewRequest("GET", os.Getenv("URL"), nil)
+	if err != nil {
+		return "", err
+	}
+
+	ageCookie := http.Cookie{
+		Name:  "age-gated",
+		Value: os.Getenv("AGE_COOKIE"),
+	}
+	req.AddCookie(&ageCookie)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	return sniffImage(body)
 }
 
 func errorResponse(wPointer *http.ResponseWriter, reason error) {
